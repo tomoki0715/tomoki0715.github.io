4a192b41c706728e5d726957ea6294bfcd2c1343
Tomoki Fukamizu <108712041+tomoki0715@users.noreply.github.com>
2023-08-05 07:49:47 +0900

    Update language.js

--- language.js
+++ language.js
@@ -1,30 +1,19 @@
-// 言語切り替え用の関数を定義
-function toggleLanguage() {
-    const htmlElement = document.querySelector('html');
-    const portfolioTitle = document.getElementById('portfolio-title');
-    const links = document.querySelectorAll('nav a');
-    const aboutSection = document.getElementById('about');
-    // 他のセクションも同様に変更してください
 
-    // 現在の言語を取得
-    const currentLanguage = htmlElement.lang;
-    // 言語を切り替える
-    if (currentLanguage === 'en') {
-        htmlElement.lang = 'ja';
-        portfolioTitle.textContent = '私のポートフォリオ';
-        links.forEach(link => link.textContent = link.dataset.jp);
-        aboutSection.querySelector('h2').textContent = aboutSection.querySelector('h2').dataset.jp;
-        aboutSection.querySelector('p').textContent = aboutSection.querySelector('p').dataset.jp;
-        // 他のセクションも同様に変更してください
+let currentLang = 'en';
+
+function switchLanguage() {
+  const elements = document.querySelectorAll('[data-lang-ja]');
+  elements.forEach((element) => {
+    const jaText = element.getAttribute('data-lang-ja');
+    const enText = element.textContent;
+    if (currentLang === 'en') {
+      element.textContent = jaText;
     } else {
-        htmlElement.lang = 'en';
-        portfolioTitle.textContent = 'My Portfolio';
-        links.forEach(link => link.textContent = link.dataset.en);
-        aboutSection.querySelector('h2').textContent = aboutSection.querySelector('h2').dataset.en;
-        aboutSection.querySelector('p').textContent = aboutSection.querySelector('p').dataset.en;
-        // 他のセクションも同様に変更してください
+      element.textContent = enText;
     }
+  });
+
+  currentLang = currentLang === 'en' ? 'ja' : 'en';
 }
 
-// ページロード時に言語切り替え用の関数を実行（デフォルトは英語バージョン）
-toggleLanguage();
+document.getElementById('lang-switch').addEventListener('click', switchLanguage);
